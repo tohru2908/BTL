4b6d2004a277f7fa0a2af0bdf322dfa90dd213cf
PA <116648516+DorasPA@users.noreply.github.com>
2022-11-20 23:53:54 +0700

    Update danhsachnhanvien.cpp

--- danhsachnhanvien.cpp
+++ danhsachnhanvien.cpp
@@ -26,6 +26,8 @@ struct dslkk{
 	dslkk();
 	node *createnode(NV x);
 	void addlast(NV x);
+	void addBefore(NV p, node *y);
+	void addAfter(NV p, node *y);
 	void insert(NV x);
 	void addNewStaff(NV x);
 	void searchStaffByPos(int pos);
@@ -51,6 +53,30 @@ node *dslkk::createnode(NV x){
 		return p;
 	}
 }
+void dslkk::addBefore(NV p, node *y){
+//them node x co data=p vao truoc node y;
+	node *x=createnode(p);
+	if (y!=head){
+		x->prev=y->prev;
+		y->prev->next=x;
+	}
+	else head=x;
+	x->next=y;
+	y->prev=x;
+	size++;
+}
+
+void dslkk::addAfter(NV p, node *y){
+	node *x=createnode(p);
+	if(tail!=y){
+		y->next->prev=x;
+		x->next=y->next;
+	}
+	else tail=x;
+	x->prev=y;
+	y->next=x;
+	size++;
+}
 void dslkk::insert(NV x){
 	if(head==NULL)
 		cout<<"danh sach trong\n";
@@ -67,8 +93,6 @@ void dslkk::insert(NV x){
 	strcpy(x.name,this->takename+0);
 	//cout<<"test name:"<<x.name<<endl;
 	addNewStaff(x);
-	
-	luong(x);
 }
 void dslkk::getName(NV x){
 	
@@ -80,111 +104,18 @@ void dslkk::getName(NV x){
 		}
 	}
 }
-void dslkk::addNewStaff(NV x){
-	char f[1000]="",g[1000]="";
-	node *p=createnode(x);
-//	tao node p nam o vi tri cuoi
 
-	if (tail!=NULL){
-	tail->next=p;
-	p->prev=tail;}
-	tail=p;
-	size++;
-//	strcpy(f,p->data.name);
-//	strcpy(g,p->prev->data.name);
-//	danh sach co nhieu hon 1 phan tu thi bat dau cho sap xep
-	if(size>=2){
-		for(int i=0;i<size;i++){
-//	quet tu cuoi len dau
-//	strcmp la lenh de so sanh 2 chuoi
-			if(strcmp(p->data.name,p->prev->data.name)>0){
-//	i=index-1 la truong hop doi cho o vi tri cuoi cung co nghia la o vi tri head
-				if(i==size-1){
-					//if(p->next!=NULL){
-					p->next->prev=p->prev;
-					p->prev->next=p->next;
-					p->next=p->prev;
-					head=p;
-				}
-//	i=0 l? truong hop doi cho ngay tai vi tri tail
-				else if(i==0&&size>2){
-					p->prev=p->prev->prev;
-					p->prev->next=p;
-					tail->prev=p;
-					tail->next=NULL;
-				}
-				else if(i==0&&size==2){
-					p->prev=p->prev->prev;
-					p->prev->next=p;
-					head=p;
-					tail->prev=p;
-					tail->next=NULL;
-				}
-				else{
-					p->next->prev=p->prev;
-					p->prev->next=p->next;
-					p->next=p->prev;
-					p->prev=p->prev->prev;
-					p->prev->next=p;
-					p->next->prev=p;
-				};
-			}else if(i==0&&size==1){
-				head=tail=p;
-				break;
-			}else if(i==0&&size>1){
-				tail=p;
-				break;
-			}
-		}
+void dslkk::addNewStaff(NV x){
+	node *p=createnode(x);
+	if (head==NULL) {head=tail=p; size++;}
+	
+	for (node *i=head;i!=NULL;i=i->next){
+		if(strcmp(p->data.name,i->data.name)<0) //xem them ve ham strcmp
+			{ addBefore(p->data,i); break;}
+		else if(tail==i&&size>1) addAfter(p->data,i);
 	}
-	else head=tail=p;
-}
-node* dslkk::luong(NV x){
-	node*p=createnode(x);
-	cout<<"luong theo hop dong: ";
-	cin>>p->data.LHD;
-	cout<<"KPI: ";
-	cin>>p->data.KPI;
-	p->data.LCD=p->data.LHD*0.25;
-	p->data.LBD=p->data.LHD*0.75;
-	p->data.total=p->data.LCD+(p->data.KPI*p->data.LBD);
-	return p;
-}
-void dslkk::searchStaffByPos(int pos){
-	if(pos>size){
-	cout<<"khong co nhan vien nao o vi tri ban can tim!";
-	}else{
-		node*p=new node;
-		p=head;
-		for(int i=0;i<=size;i++){
-		if(i==pos){
-			cout<<p;
-		}else {
-			p=p->next;
-		}
-	}
-	}
-}
-void dslkk::removeStaffByPos(int pos){
-//	Node*p=searchStaffByPos(pos);
-	if(pos>size){
-	cout<<"khong co nhan vien nao o vi tri ban can xoa!";
-	}else{
-		node*p=new node;
-		p=head;
-		for(int i=0;i<=size;i++){
-		if(i==pos){
-			p->prev->next = p->next;
-    		p->next->prev = p->prev;
-    		p->prev = NULL;
-    		p->next = NULL;
-    		delete p;
-		}else {
-			p=p->next;
-		}
-	}
-	}
-}
+	
+
 void dslkk::print(){
 	for(node *i=head; i!=NULL; i=i->next)
 	cout<<i->data.name<<"\t"<<i->data.MSNV<<"\t"<<i->data.loaiSP<<"\t"<<i->data.total<<endl;}
@@ -197,4 +128,3 @@ int main(){
 	l.insert(NV3);
 	l.print();
 }
-
